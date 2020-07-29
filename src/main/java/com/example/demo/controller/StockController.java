c8481b27777989466b5d895abc19b68526c60609
duyvl <duyvl@novaon.vn>
2020-07-29 16:02:57 +0700

    init

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StockController {
     @GetMapping("/")
-    public String list() {
-        return "products";
+    public String stock() {
+        return "index";
     }
 }
